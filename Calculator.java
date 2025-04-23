4b42a3e0b11c13604b1768ff0d41bbcb5fb5aa61
grk-web <karangaleganesh100@gmail.com>
2025-04-23 21:35:45 +0530

    Add files via upload
    A java program to design  a calculator using switch statement

---
+++ Calculator.java
@@ -0,0 +1,49 @@
+package pack;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Getting user input
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Choose an operation: +, -, *, /");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        // Using switch statement to perform the operation
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator! Please use +, -, *, or /.");
+        }
+
+        scanner.close();
+    }
+}
